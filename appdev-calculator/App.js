ef0a373522845ba05ad816ee1cc36ed23ccfc85d
tulisamarth <39885124+tulisamarth@users.noreply.github.com>
2019-04-30 11:36:05 -0400

    Used Scorekeeper Field Goal from CodeHS to for calculator app example.

--- appdev-calculator/App.js
+++ appdev-calculator/App.js
@@ -1,21 +1,117 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { AppRegistry, Text, View, TouchableHighlight, StyleSheet } from 'react-native';
+import { Constants } from 'expo';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
+export default class App extends Component {
+   state = {
+        teamOneScore: 0,
+        teamTwoScore: 0,
+    }
+
+    teamOneTouchdown = () => {
+        this.setState({teamOneScore: this.state.teamOneScore + 7,})
+    }
+
+    teamOneFieldGoal = () => {
+        this.setState({teamOneScore: this.state.teamOneScore + 3,})
+    }
+
+    teamTwoTouchdown = () => {
+        this.setState({teamTwoScore: this.state.teamTwoScore + 7,})
+    }
+
+    teamTwoFieldGoal = () => {
+        this.setState({teamTwoScore: this.state.teamTwoScore + 3,})
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.paragraph}>
+                    FOOTBALL SCOREKEEPER
+                </Text>
+
+                <View style={styles.buttonContainer}>
+                    <TouchableHighlight
+                        style={styles.button}
+                        onPress = {this.teamOneTouchdown}
+                    >
+                        <Text style={styles.buttonText}>
+                            TEAM 1 TOUCHDOWN!
+                        </Text>
+                    </TouchableHighlight>
+                    <TouchableHighlight
+                        style={styles.button}
+                        onPress = {this.teamOneFieldGoal}
+                    >
+                        <Text style={styles.buttonText}>
+                            TEAM 1 FIELD GOAL!
+                        </Text>
+                    </TouchableHighlight>
+                </View>
+
+                <View style={styles.buttonContainer}>
+                    <TouchableHighlight
+                        style={styles.button}
+                        onPress = {this.teamTwoTouchdown}
+                    >
+                        <Text style={styles.buttonText}>
+                            TEAM 2 TOUCHDOWN!
+                        </Text>
+                    </TouchableHighlight>
+
+                    <TouchableHighlight
+                        style={styles.button}
+                        onPress = {this.teamTwoFieldGoal}
+                    >
+                        <Text style={styles.buttonText}>
+                            TEAM 2 FIELD GOAL!
+                        </Text>
+                    </TouchableHighlight>
+                </View>
+
+                <Text style={styles.paragraph}>
+                    Team 1:  {this.state.teamOneScore}
+                </Text>
+
+                <Text style={styles.paragraph}>
+                    Team 2:  {this.state.teamTwoScore}
+                </Text>
+
+            </View>
+      );
+   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'green',
+    },
+    paragraph: {
+        color: 'white',
+        fontSize: 20,
+        textAlign: 'center',
+        fontWeight: 'bold',
+        marginTop: 10
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+
+    },
+    button: {
+        height: 50,
+        width: 80,
+        backgroundColor: 'green',
+        borderColor: 'white',
+        borderWidth: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 10,
+    },
 });
